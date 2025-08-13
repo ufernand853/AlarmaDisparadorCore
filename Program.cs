b610dedd36c870fbfad163f0d21c25acb240195b
ufernand853 <ufernand853@gmail.com>
2025-08-13 16:07:26 -0300

    Read interval without GetValue

--- Program.cs
+++ Program.cs
@@ -1,5 +1,26 @@
 using AlarmaDisparadorCore.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading;
 
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var intervaloSegundos = 60;
+if (int.TryParse(config["IntervaloSegundos"], out var parsedIntervalo))
+{
+    intervaloSegundos = parsedIntervalo;
+}
 var evaluador = new Evaluador();
-evaluador.EvaluarReglas();
+
+while (true)
+{
+    Console.WriteLine($"Inicio de evaluación: {DateTime.Now:O}");
+    evaluador.EvaluarReglas();
+    Console.WriteLine($"Fin de evaluación: {DateTime.Now:O}");
+    Thread.Sleep(TimeSpan.FromSeconds(intervaloSegundos));
+}
 
