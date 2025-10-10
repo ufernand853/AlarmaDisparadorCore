61d384f1b4ba553eea53267d384ec7479ec96d13
ufernand853 <ufernand853@gmail.com>
2025-10-10 17:50:06 -0300

    Disable default SMTP credentials when custom credentials provided

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -105,6 +105,7 @@ namespace AlarmaDisparadorCore.Services
 
                 if (!string.IsNullOrWhiteSpace(_userName) && !string.IsNullOrWhiteSpace(_password))
                 {
+                    client.UseDefaultCredentials = false;
                     client.Credentials = new NetworkCredential(_userName, _password);
                 }
                 else
